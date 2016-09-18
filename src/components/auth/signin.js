82ac6c88b8b43020e39b15b9c0c58f123ba47ee8
Stephen Mayeux <stephenmayeux@gmail.com>
2016-09-18 16:12:50 -0400

    revert to older version of redux-form

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 
-class Sign extends Component {
+class Signin extends Component {
+  handleFormSubmit({email, password}) {
+    console.log(email, password);
+  }
+
   render() {
+
+    const { handleSubmit, fields: { email, password }} = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
         <fieldset className="form-group">
           <label>Email:</label>
-          <input className="form-control" />
+          <input {...email} className="form-control" />
         </fieldset>
         <fieldset className="form-group">
           <label>Password:</label>
-          <input className="form-control" />
+          <input {...password} className="form-control" />
         </fieldset>
+        <button action="submit" className="btn btn-primary">Sign In</button>
       </form>
-      <button action="submit" className="btn btn-primary">Sign In</button>
     );
   }
 }
