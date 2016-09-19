8ab495ac2d7537ca6724cedda019b648127a708c
Stephen Mayeux <stephenmayeux@gmail.com>
2016-09-19 09:52:48 -0400

    fix signin form bug

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -3,7 +3,7 @@ import { reduxForm } from 'redux-form';
 
 class Signin extends Component {
   handleFormSubmit({email, password}) {
-    console.log(email, password);
+    console.log(this.props);
   }
 
   render() {
