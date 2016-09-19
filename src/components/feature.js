cd2bfce66f4c07aaaf465335dcf8b910f277f0ba
Stephen Mayeux <stephenmayeux@gmail.com>
2016-09-19 16:47:29 -0400

    protect secret route

---
+++ src/components/feature.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Feature extends Component {
+  render() {
+    return (
+      <h3>Super secret feature for users only!</h3>
+    );
+  }
+}
+
+export default Feature;
