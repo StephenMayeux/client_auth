379a4d44120ec53b6b2d7ac7a55b269a7be3872b
Stephen Mayeux <stephenmayeux@gmail.com>
2016-09-19 11:52:47 -0400

    update header based on authenticated state

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderMenuItems() {
+    if (this.props.authenticated) {
+      return (
+        <ul className="nav navbar-nav">
+          <li className="nav-item">Log Out</li>
+        </ul>
+      );
+    } else {
+      return (
+        <ul className="nav navbar-nav">
+          <li className="nav-item">Sign In</li>
+          <li className="nav-item">Sign Up</li>
+        </ul>
+      );
+    }
+  }
+
   render() {
     return (
       <nav className="navbar navbar-light">
-        <ul className="nav navbar-nav">
-          <li className="nav-item">Sign In</li>
-        </ul>
+        {this.renderMenuItems()}
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return { authenticated: state.auth.authenticated }
+}
+
+export default connect(mapStateToProps)(Header);
